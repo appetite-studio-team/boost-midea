e69acbea211d7ed321f24bf403c8e41fd39f3bee
4samad <smd2k15@gmail.com>
2024-09-28 16:33:24 +0530

    :zap: optimize images

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+  // output: 'export',
+  // images: { unoptimized: true },
 };
 
 export default nextConfig;
