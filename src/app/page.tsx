e54e8f71dc3c4b558c4e1fb62f98c05106d128f9
4samad <smd2k15@gmail.com>
2024-09-28 19:35:40 +0530

    :lipstick: update home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <>
       <HeroSection />
       <ServicesSection />
-      <TestimonialsSection />
       <PortfolioSection />
+      <TestimonialsSection />
       <ContactSection />
     </>
   );
