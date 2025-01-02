0c52e2c942bd95fe18fbba6a0c557d419c5fa470
4samad <smd2k15@gmail.com>
2025-01-02 23:44:05 +0530

    :adhesive_bandage: update env name

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -29,7 +29,7 @@ export default buildConfig({
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
   db: mongooseAdapter({
-    url: process.env.DATABASE_URI || '',
+    url: process.env.NEXT_PUBLIC_DATABASE_URI || '',
   }),
   sharp,
   plugins: [
